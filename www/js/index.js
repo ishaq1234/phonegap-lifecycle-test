0f565e972158917b9bd3946dde175ef251d358e3
ishaq1234 <44059576+ishaq1234@users.noreply.github.com>
2018-11-01 13:30:21 +0000

    storage
    storage

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,25 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
 
-document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
-function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
-}
-
-
-// device APIs are available
+//document.addEventListener("deviceready", onDeviceReady, false);
 //
-    function onDeviceReady() {
-	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	updateDisplay();
-	    
-	alert("device ready");
-    }
+//// device APIs are available
+//function onDeviceReady() {
+//
+//
+//    alert("device ready");
+//}
 
 
-    function onPause() {
-	
-	paused_count++;
-	updateDisplay();
-	    
-	alert("pause");
-    }
-	
 
-    function onResume() {
-		
-	resumed_count++;
-	updateDisplay();
-	    
-	alert("resume");
-    }
+$( document ).ready(function() {
+    
+    var key = "pen";
+    var value ="blue";
+    
+    window.localstorage.setitem(key, value );
+    
+    var key = "pen";
+    var value = window.localstorage.getitem(key); 
+    
+
+    console.log( "ready!" );
+});
